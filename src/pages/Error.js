7cb4591344dfaeaf46bea387c292ac7a36061365
matheus <matheus.massari@protonmail.com>
2022-02-06 13:20:58 -0300

    repos setup

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Página não encontrada :(</h3>
+        <Link to="/" className="btn">
+          Voltar
+        </Link>
+      </section>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
